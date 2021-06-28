b4d0696e30b0dea492ed5ae90a3ac08300e7a125
max <max.assis@ymail.com>
2021-06-28 14:06:59 -0300

    corrigindo bugs

--- src/templates/User/index.js
+++ src/templates/User/index.js
@@ -1,10 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import GithubCorner from "react-github-corner";
+import Lottie from "react-lottie";
+import animationData from '../../assets/imgs/back.json'
 import useStore from "../../services/store";
 import CardUser from "../../components/CardUser";
+import { Link } from 'react-router-dom'
 import api from "../../services/api";
 import SingleCardRepositorie from '../../components/CardSingleRepositorie/index'
+import styles from './styles.module.scss'
+
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 function User() {
   const [repositories, setRepositories] = useState();
@@ -42,6 +56,17 @@ function User() {
 
   return (
     <div className="container">
+    
+    <div className={styles.lottie}>
+     <Link to="/" >
+      <Lottie 
+	    options={defaultOptions}
+        height={35}
+        width={35}
+      />
+      </Link> 
+      </div>
+    
       <CardUser 
       setToggleRepositories={setToggleRepositories} 
       toggleRepositories={toggleRepositories} 
